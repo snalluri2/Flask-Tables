7944e86fa0c512e7c66b74b08d7a9997bd821ab5
snalluri2 <srikarnllr@gmail.com>
2021-12-14 08:22:01 -0500

    Task :- Making corrections

--- app.py
+++ app.py
@@ -1,28 +1,11 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True)
-    age = db.Column(db.Integer, index=True)
-    address = db.Column(db.String(256))
-    phone = db.Column(db.String(20))
-    email = db.Column(db.String(120), index=True)
-
-db.create_all()
 
 
 @app.route('/')
-def index():
-    users = User.query
-    return render_template('basic_table.html', title='Basic Table',
-                           users=users)
+def hello_world():  # put application's code here
+    return 'Hello World!'
 
 
 if __name__ == '__main__':
